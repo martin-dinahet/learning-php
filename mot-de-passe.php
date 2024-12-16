f803957d097af33f352dc82d3980980e7f865f81
Martin <martin.dinahet@proton.me>
2024-12-16 09:22:35 +0100

    added mot-de-passe

---
+++ mot-de-passe.php
@@ -0,0 +1,13 @@
+<?php
+
+$generate_password = function($length) {
+  $characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,<.>/?!@#$%^&*()-_=+|";
+  $final_password = "";
+  for ($i = 0; $i < $length; $i++) {
+    $final_password .= $characters[rand(0, strlen($characters))];
+  }
+  return $final_password;
+};
+
+
+echo $generate_password(8);
